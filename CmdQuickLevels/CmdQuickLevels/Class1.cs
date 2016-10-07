35d050c530efe50273b32ae0e53a0fe69af2f7d5
McCulloughRT <McCulloughRT@gmail.com>
2016-10-07 15:07:29 -0700

    Abstracted functionality into methods

--- CmdQuickLevels/CmdQuickLevels/Class1.cs
+++ CmdQuickLevels/CmdQuickLevels/Class1.cs
@@ -15,33 +15,66 @@ namespace QuickLevels
 
     public class CmdQuickLevels : IExternalCommand
     {
+        private Document _doc;
+        private ElementId _floorPlanType;
+        private ElementId _ceilingPlanType;
         public Result Execute(ExternalCommandData commandData,
             ref string message, ElementSet elements)
 
         {
+            _doc = commandData.Application.ActiveUIDocument.Document;
+            _floorPlanType = GetViewFamily(ViewFamily.FloorPlan);
+            _ceilingPlanType= GetViewFamily(ViewFamily.CeilingPlan);
 
-            UIApplication app = commandData.Application;
-            Document doc = app.ActiveUIDocument.Document;
+            double offset = 20;
+            double maxLevel = GetMaxLevel() + offset;
 
+            Result result = CreateLevel(maxLevel);
+            return result;
+        }
+
+        private ElementId GetViewFamily(ViewFamily familyType)
+        {
             IEnumerable<ViewFamilyType> viewFamilyFloorPlan =
-               from elem in new FilteredElementCollector(doc)
+               from elem in new FilteredElementCollector(_doc)
                .OfClass(typeof(ViewFamilyType))
                let type = elem as ViewFamilyType
-               where type.ViewFamily == ViewFamily.FloorPlan
+               where type.ViewFamily == familyType
                select type;
+            return viewFamilyFloorPlan.First().Id;
+        }
 
+        private Result CreateLevel(double level)
+        {
+            using (Transaction tx = new Transaction(_doc))
+            {
+                tx.Start("Create New Level");
+                try
+                {
+                    Level newLevel = _doc.Create.NewLevel(level);
+                    if (null == newLevel)
 
-            IEnumerable<ViewFamilyType> viewFamilyCeilingPlan =
-               from elem in new FilteredElementCollector(doc)
-               .OfClass(typeof(ViewFamilyType))
-               let type = elem as ViewFamilyType
-               where type.ViewFamily == ViewFamily.CeilingPlan
-               select type;
+                        newLevel.Name = "New Name";
 
+                    ViewPlan.Create(_doc, _floorPlanType, newLevel.Id);
+                    ViewPlan.Create(_doc, _ceilingPlanType, newLevel.Id);
+
+                    tx.Commit();
+                }
+                catch (Autodesk.Revit.Exceptions.OperationCanceledException)
+                {
+                    return Result.Cancelled;
+                }
+            }
+            return Result.Succeeded;
+        }
+
+        private double GetMaxLevel()
+        {
             IList<double> myValue = new List<double>();
             ElementCategoryFilter modelLevel = new ElementCategoryFilter(BuiltInCategory.OST_Levels);
 
-            FilteredElementCollector coll = new FilteredElementCollector(doc);
+            FilteredElementCollector coll = new FilteredElementCollector(_doc);
             IList<Element> levelList = coll.WherePasses(modelLevel).ToElements();
             foreach (Element e in levelList)
             {
@@ -54,32 +87,7 @@ namespace QuickLevels
                 double paramValue = parameterLevel.AsDouble();
                 myValue.Add(paramValue);
             }
-            double maxValue = myValue.Max();
-
-            double elevation = 20.0;
-
-
-            using (Transaction tx = new Transaction(doc))
-            {
-                tx.Start("Create New Level");
-                try
-                {
-                    Level level = doc.Create.NewLevel(maxValue + elevation);
-                    if (null == level)
-
-                        level.Name = "New Name";
-
-                    ViewPlan.Create(doc, viewFamilyFloorPlan.First().Id, level.Id);
-                    ViewPlan.Create(doc, viewFamilyCeilingPlan.First().Id, level.Id);
-
-                    tx.Commit();
-                }
-                catch (Autodesk.Revit.Exceptions.OperationCanceledException)
-                {
-                    return Result.Cancelled;
-                }
-            }
-            return Result.Succeeded;
+            return myValue.Max();
         }
 
     }
